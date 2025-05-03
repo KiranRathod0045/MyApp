33f954a4c4542c09550e2324e1c4e01eb8692761
DESKTOP-QC6C26E\Kiran Rathod <kiran.r@cheriseglobl.com>
2025-05-03 14:43:45 +0530

    params

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5005
 app.listen(PORT,()=>{
     console.log(`running on ${PORT}`)
 })
